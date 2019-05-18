7931c127ac34c05497b13032349bab31c76388b9
Mario <laserna.mario@gmail.com>
2019-05-18 11:23:23 -0500

    se organiza estructra en archivo de funciones

---
+++ funciones.js
@@ -0,0 +1,18 @@
+function cursoToString(curso) {
+    return '----------------------------\n' +
+        'Id: ' + curso.id + ' - Nombre: ' + curso.nombre + '\n' +
+        'Duración: ' + curso.duracion + ' - Valor: ' + curso.valor;
+}
+
+function listadoConRetraso(cursos) {
+    for (let i = 0; i < cursos.length; i++) {
+        setTimeout(() => {
+            console.log(cursoToString(cursos[i]));
+        }, i * 2000);
+    }
+}
+
+module.exports = {
+    cursoToString,
+    listadoConRetraso
+}
--- principal.js
+++ principal.js
@@ -1,18 +1,4 @@
 const datos = require('./cursos');
+const funciones = require('./funciones');
 
-function cursoToString(curso) {
-    return '----------------------------\n' +
-        'Id: ' + curso.id + ' - Nombre: ' + curso.nombre + '\n' +
-        'Duración: ' + curso.duracion + ' - Valor: ' + curso.valor;
-}
-
-function listadoConRetraso(cursos) {
-    for (let i = 0; i < cursos.length; i++) {
-        setTimeout(() => {
-            console.log(cursoToString(cursos[i]));
-        }, i * 2000);
-    }
-}
-
-
-listadoConRetraso(datos.cursos);
+funciones.listadoConRetraso(datos.cursos);
